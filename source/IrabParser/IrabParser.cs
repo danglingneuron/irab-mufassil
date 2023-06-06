ecc7d4ce80908ac41c9faad8e8c06eee6b498c6a
Imran Shafiq <emi_oasis@hotmail.com>
2023-06-06 13:19:10 -0500

    refactor cleanup

--- source/IrabParser/IrabParser.cs
+++ source/IrabParser/IrabParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,29 +12,10 @@ namespace Irab
     public class IrabParser
     {
         const string LineBreak = "\n"; //<br>
-        string[] ArabicNumerals = new string[]
-        {
-            "٠","١","٢","٣","٤","٥","٦","٧","٨","٩"
-        };
-        int ConvertArabicNumeral(char[] input)
-        {
-            int output = 0;
-            int power = 0;
-            for (int i = 0; i < input.Length; i++)
-            {
-                int digit = Array.IndexOf<string>(ArabicNumerals, input[i].ToString());
-                if (digit == -1)
-                {
-                    continue;
-                }
-                output += digit * (int)Math.Pow(10, power);
-                power++;
-            }
-            return output;
-        }
+        
 
         int surahId = 0;
-        int ayahId = 0;
+        
         public List<AyahIrab> Parse(string rootPath)
         {
             List<AyahIrab> irabAyahs = new List<AyahIrab>();
@@ -42,15 +24,14 @@ namespace Irab
             {
                 string vname = rootPath + vi.ToString("000") + ".htm";
                 string volume = File.ReadAllText(vname);
-                var pages = ExtractPages(volume);
+                var pages = Utils.ExtractPages(volume);
                 AyahIrab? irabAyah = null;
                 foreach (var page in pages)
                 {
-                    var lines = ExtractLines(page);
+                    var lines = Utils.ExtractLines(page);
                    
-                    for (int il = 0; il < lines.Length; il++)
+                    foreach (var line in lines)
                     {
-                        var line = lines[il];
                         var rawLine = line.Trim();
                         if (string.IsNullOrEmpty(rawLine) || rawLine == "\u200C")
                         {
@@ -62,35 +43,30 @@ namespace Irab
                             irabAyah = null;
                             continue;
                         }
-                        if (rawLine.StartsWith("[") && rawLine.Contains("سورة") && rawLine.Contains("آية"))
+                        if(Regex.IsMatch(rawLine, "\\[.*?(سورة).*?(آية)")) 
                         {
-                            ayahId++;
                             irabAyah = new AyahIrab() {  WordGroups = new List<WordGroupIrab>() };
                             irabAyahs.Add(irabAyah);
                             var saText = rawLine.Replace("]", "").Replace("[", "").Replace("سورة ", "").Replace(" آية ", "").Replace("(", "").Replace(")", "");
                             var saSplit = saText.Split(" ");
                             var snumeral = saSplit[saSplit.Length - 1].Split(":");
-                            var sid = ConvertArabicNumeral(snumeral[0].Reverse().ToArray());
-                            var aid = ConvertArabicNumeral(snumeral[1].Reverse().ToArray());
-                            surahId = sid;
-                            irabAyah.Id = $"{sid}:{aid}";
-                            irabAyah.Location = new[] { sid, aid };
+                            irabAyah.Location = new[] { Utils.ConvertArabicNumeral(snumeral[0]), Utils.ConvertArabicNumeral(snumeral[1]) };
+                            irabAyah.Id = $"{irabAyah.Location[0]}:{irabAyah.Location[1]}";
+                            surahId = irabAyah.Location[0];
                             continue;
                         }
                         if (Regex.IsMatch(rawLine, "(\\()?[٠١٢٣٤٥٦٧٨٩]+(\\))?\\s+{.+?}"))
                         {
-                            ayahId++;
                             irabAyah = new AyahIrab() { WordGroups = new List<WordGroupIrab>() };
                             irabAyahs.Add(irabAyah);
                             var ayah = Regex.Replace(rawLine, "{.+?}", "").Replace("(", "").Replace(")", "").Replace(".", "").Replace("*", "").Trim();
-                            var aid = ConvertArabicNumeral(ayah.Reverse().ToArray());
+                            var aid = Utils.ConvertArabicNumeral(ayah);
                             if (aid == 1)
                             {
                                 surahId++;
                             }
-                            var sid = surahId;
-                            irabAyah.Id = $"{sid}:{aid}";
-                            irabAyah.Location = new[] { sid, aid };
+                            irabAyah.Id = $"{surahId}:{aid}";
+                            irabAyah.Location = new[] { surahId, aid };
                             irabAyah.Arabic = rawLine;
                             continue;
                         }
@@ -124,9 +100,9 @@ namespace Irab
                         if (!string.IsNullOrEmpty(irabAyah.Arabic))
                         {
                             if (string.IsNullOrEmpty(irabAyah.Grammar))
-                                irabAyah.Grammar = ProcessLine(rawLine);
+                                irabAyah.Grammar = rawLine;
                             else
-                                irabAyah.Grammar += LineBreak + ProcessLine(rawLine);
+                                irabAyah.Grammar += LineBreak + rawLine;
                         }
                         if (rawLine.StartsWith("•"))
                         {
@@ -134,7 +110,7 @@ namespace Irab
                             {
                                 continue;
                             }
-                            var wordGroup = ExtractWordGroup( rawLine, true);
+                            var wordGroup = Utils.ExtractWordGroup( rawLine, true);
                             if (wordGroup != null)
                                 irabAyah.WordGroups.Add(wordGroup);
                             continue;
@@ -142,7 +118,7 @@ namespace Irab
                         if (irabAyah.WordGroups?.Count > 0)
                         {
                             var currentWord = irabAyah.WordGroups.Last();
-                            currentWord.Grammar += LineBreak + ProcessLine(rawLine);
+                            currentWord.Grammar += LineBreak + rawLine;
                         }
                         else
                         {
@@ -150,7 +126,7 @@ namespace Irab
                             {
                                 continue;
                             }
-                            var wordGroup = ExtractWordGroup(rawLine, false);
+                            var wordGroup = Utils.ExtractWordGroup(rawLine, false);
                             if (wordGroup != null)
                                 irabAyah.WordGroups.Add(wordGroup);
                         }
@@ -162,40 +138,7 @@ namespace Irab
         }
 
         
-        string[] ExtractPages(string volumText)
-        {
-            var cleanText = volumText.Trim();
-            cleanText = Regex.Replace(cleanText, "<!DOCTYPE(.|\\n)+?<body>", "");
-            cleanText = Regex.Replace(cleanText, "<div class='Main'>", "");
-            cleanText = Regex.Replace(cleanText, "</div></body></html>", "");
-            var pages = cleanText.Trim().Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-            return pages;
-        }
-        string[] ExtractLines(string pageText)
-        {
-            var cleanText = pageText.Replace("\n", "").Trim();
-            cleanText = Regex.Replace(cleanText, "<div class='PageHead'>.+?<\\/div>", "");
-            cleanText = Regex.Replace(cleanText, "<span class=\"title\">.+?<\\/span>", "");
-            cleanText = Regex.Replace(cleanText, "<font.+?>|<\\/font>", "");
-            cleanText = Regex.Replace(cleanText, "<div class='PageText'>", "");
-            cleanText = Regex.Replace(cleanText, "</div>", "");
-            cleanText = Regex.Replace(cleanText, "</p>", "\n");
-            cleanText = Regex.Replace(cleanText, "]", "]\n");
-            var lines = cleanText.Trim().Split("\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-            return lines;
-        }       
-        WordGroupIrab ExtractWordGroup(string rawLine, bool dotted)
-        {
-            var colon = rawLine.IndexOf(":");
-            string words = dotted? rawLine.Substring(1, colon - 1).Trim(): rawLine.Substring(0, colon).Trim();
-            words = words.Replace("{", "").Replace("}", "").Replace("\u200D", "").Trim();
-            return new WordGroupIrab() { Words = words, Grammar = ProcessLine(rawLine) };
-
-        }
-        string ProcessLine(string lineText)
-        {
-            return lineText.Trim();
-        }
+        
     }
 }
 
---
+++ source/IrabParser/Utils.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Irab
+{
+    public class Utils
+    {
+        static string[] ArabicNumerals = new string[]
+        {
+            "٠","١","٢","٣","٤","٥","٦","٧","٨","٩"
+        };
+        
+        public static int ConvertArabicNumeral(string arabic)
+        {
+            var input = arabic.Reverse().ToArray();
+            int output = 0;
+            int power = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int digit = Array.IndexOf<string>(ArabicNumerals, input[i].ToString());
+                if (digit == -1)
+                {
+                    throw new ArgumentException("error.arabicnumeral");
+                }
+                output += digit * (int)Math.Pow(10, power);
+                power++;
+            }
+            return output;
+        }
+
+        public static string[] ExtractPages(string volumText)
+        {
+            var cleanText = volumText.Trim();
+            cleanText = Regex.Replace(cleanText, "<!DOCTYPE(.|\\n)+?<body>", "");
+            cleanText = Regex.Replace(cleanText, "<div class='Main'>", "");
+            cleanText = Regex.Replace(cleanText, "</div></body></html>", "");
+            var pages = cleanText.Trim().Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            return pages;
+        }
+        public static string[] ExtractLines(string pageText)
+        {
+            var cleanText = pageText.Replace("\n", "").Trim();
+            cleanText = Regex.Replace(cleanText, "<div class='PageHead'>.+?<\\/div>", "");
+            cleanText = Regex.Replace(cleanText, "<span class=\"title\">.+?<\\/span>", "");
+            cleanText = Regex.Replace(cleanText, "<font.+?>|<\\/font>", "");
+            cleanText = Regex.Replace(cleanText, "<div class='PageText'>", "");
+            cleanText = Regex.Replace(cleanText, "</div>", "");
+            cleanText = Regex.Replace(cleanText, "</p>", "\n");
+            cleanText = Regex.Replace(cleanText, "]", "]\n");
+            var lines = cleanText.Trim().Split("\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            return lines;
+        }
+        public static WordGroupIrab ExtractWordGroup(string rawLine, bool dotted)
+        {
+            var colon = rawLine.IndexOf(":");
+            string words = dotted ? rawLine.Substring(1, colon - 1).Trim() : rawLine.Substring(0, colon).Trim();
+            words = words.Replace("{", "").Replace("}", "").Replace("\u200D", "").Trim();
+            return new WordGroupIrab() { Words = words, Grammar = rawLine };
+        }
+        
+    }
+}
